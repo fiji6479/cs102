5b922c1f8b33126a3096484049a406c1f301cd9f
fiji6479 <71012423+fiji6479@users.noreply.github.com>
2020-12-20 22:07:30 +0300

    Update rsa.py

--- homework01/rsa.py
+++ homework01/rsa.py
@@ -3,64 +3,38 @@ import typing as tp
 
 
 def is_prime(n: int) -> bool:
-    """
-        >>> is_prime(2)
-        True
-        >>> is_prime(11)
-        True
-        >>> is_prime(8)
-        False
-        """
-    for i in range(2,n):
+    if n == 1: return False
+    for i in range(2,int(n**0.5)+1):
         if n % i == 0:
             return False
-
     return True
 
 
 def gcd(a: int, b: int) -> int:
-    """
-    >>> gcd(12, 15)
-    3
-    >>> gcd(3, 7)
-    1
-    """
-    if (b == 0):
+    if b == 0:
         return a
     else:
         return gcd(b, a % b)
-    pass
 
 
 def multiplicative_inverse(e: int, phi: int) -> int:
-    """
-    >>> multiplicative_inverse(7, 40)
-    23
-    """
-    n = 0
-    if phi == 1:
-        return 0
+    n=0
+    if phi == 1: return 0
     while (not ((phi * n + 1) / e).is_integer()) or ((phi * n + 1) / e == e):
         n += 1
     return int((phi * n + 1) / e)
-    pass
+
 
 def generate_keypair(p: int, q: int) -> tp.Tuple[tp.Tuple[int, int], tp.Tuple[int, int]]:
     if not (is_prime(p) and is_prime(q)):
-        raise ValueError('Both numbers must be prime.')
+        raise ValueError("Both numbers must be prime.")
     elif p == q:
-        raise ValueError('p and q cannot be equal')
-    # n = pq
+        raise ValueError("p and q cannot be equal")
     n = p*q
-
-    # phi = (p-1)(q-1)
     phi = (p-1)*(q-1)
 
-
-    # Choose an integer e such that e and phi(n) are coprime
+    # Use Euclid's Algorithm to verify that e and phi(n) are coprime
     e = random.randrange(1, phi)
-
-    # Use Euclid's Algorithm to verify that e and phi(n) are comprime
     g = gcd(e, phi)
     while g != 1:
         e = random.randrange(1, phi)
@@ -68,6 +42,7 @@ def generate_keypair(p: int, q: int) -> tp.Tuple[tp.Tuple[int, int], tp.Tuple[in
 
     # Use Extended Euclid's Algorithm to generate the private key
     d = multiplicative_inverse(e, phi)
+
     # Return public and private keypair
     # Public key is (e, n) and private key is (d, n)
     return ((e, n), (d, n))
