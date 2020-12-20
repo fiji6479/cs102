76f359f44ea17030655f0ebd9129f077223d56ec
fiji6479 <71012423+fiji6479@users.noreply.github.com>
2020-12-20 22:06:33 +0300

    Update caesar.py

--- homework01/caesar.py
+++ homework01/caesar.py
@@ -1,75 +1,50 @@
 import typing as tp
 
-
 def encrypt_caesar(plaintext: str, shift: int = 3) -> str:
-    """
-    Encrypts plaintext using a Caesar cipher.
 
-    >>> encrypt_caesar("PYTHON")
-    'SBWKRQ'
-    >>> encrypt_caesar("python")
-    'sbwkrq'
-    >>> encrypt_caesar("Python3.6")
-    'Sbwkrq3.6'
-    >>> encrypt_caesar("")
-    ''
-    """
     ciphertext = ""
-    for i in list(plaintext):
-
-        if i.isalpha():
-
-            if i.isupper():
-                ascii_code = ord(i) + shift
-                if ascii_code >= 91:
-                    ascii_code = (ascii_code - 90) + 64
-                ciphertext += chr(ascii_code)
+    s = 'abcdefghijklmnopqrstuvwxyz'
+    b = 'ABCDEFGHIJKLMNOPQRSTUVWXYZ'
+    for i in plaintext:
+        if i in s:
+            if s.find(i) + shift > 25:
+                l = s.find(i) + shift - 26
             else:
-                ascii_code = ord(i) + shift
-                if ascii_code >= 123:
-                    ascii_code = (ascii_code - 122) + 96
-                ciphertext += chr(ascii_code)
+                l = s.find(i) + shift
+            ciphertext += s[l]
+        elif i in b:
+            if b.find(i) + shift > 25:
+                l = b.find(i) + shift - 26
+            else:
+                l = b.find(i) + shift
+            ciphertext += b[l]
         else:
             ciphertext += i
-
     return ciphertext
 
-
 def decrypt_caesar(ciphertext: str, shift: int = 3) -> str:
-    """
-    Decrypts a ciphertext using a Caesar cipher.
 
-    >>> decrypt_caesar("SBWKRQ")
-    'PYTHON'
-    >>> decrypt_caesar("sbwkrq")
-    'python'
-    >>> decrypt_caesar("Sbwkrq3.6")
-    'Python3.6'
-    >>> decrypt_caesar("")
-    ''
-    """
     plaintext = ""
-    for i in list(ciphertext):
-        if i.isalpha():
-            if i.isupper():
-                ascii_code = ord(i) - shift
-                if ascii_code <= 64:
-                    ascii_code = 91 - (65 - ascii_code)
-                plaintext += chr(ascii_code)
+    s = 'abcdefghijklmnopqrstuvwxyz'
+    b = 'ABCDEFGHIJKLMNOPQRSTUVWXYZ'
+    for i in ciphertext:
+        if i in s:
+            if s.find(i) - shift < 0:
+                l = 26 + s.find(i) - shift
             else:
-                ascii_code = ord(i) - shift
-                if ascii_code <= 96:
-                    ascii_code = 123 - (97 - ascii_code)
-                plaintext += chr(ascii_code)
+                l = s.find(i) - shift
+            plaintext += s[l]
+        elif i in b:
+            if b.find(i) - shift < 0:
+                l = 26 + b.find(i) - shift
+            else:
+                l = b.find(i) - shift
+            plaintext += b[l]
         else:
             plaintext += i
     return plaintext
 
-
 def caesar_breaker_brute_force(ciphertext: str, dictionary: tp.Set[str]) -> int:
-    """
-    Brute force breaking a Caesar cipher.
-    """
+
     best_shift = 0
-    # PUT YOUR CODE HERE
     return best_shift
